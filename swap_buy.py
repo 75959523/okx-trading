c979445e7f295add3bd0af6b4f94a8228e2eb7fc
75959523 <54936383+75959523@users.noreply.github.com>
2025-05-16 07:16:40 +0800

    commit

--- swap_buy.py
+++ swap_buy.py
@@ -3,18 +3,18 @@ import concurrent.futures
 
 coins = [
     {"inst_id": "BTC-USDT-SWAP", "leverage": 30, "notional": 500, "pos_side": "long"},
+    {"inst_id": "ETH-USDT-SWAP", "leverage": 15, "notional": 500, "pos_side": "long"},
     {"inst_id": "SOL-USDT-SWAP", "leverage": 15, "notional": 500, "pos_side": "long"},
-    {"inst_id": "PEPE-USDT-SWAP", "leverage": 15, "notional": 500, "pos_side": "long"},
+    {"inst_id": "PEPE-USDT-SWAP", "leverage": 10, "notional": 500, "pos_side": "long"},
 
-    # {"inst_id": "ETH-USDT-SWAP", "leverage": 15, "notional": 500, "pos_side": "long"},
-    # {"inst_id": "ADA-USDT-SWAP", "leverage": 15, "notional": 500, "pos_side": "long"},
-    # {"inst_id": "SUI-USDT-SWAP", "leverage": 15, "notional": 500, "pos_side": "long"},
-    # {"inst_id": "AAVE-USDT-SWAP", "leverage": 15, "notional": 500, "pos_side": "long"},
-    # {"inst_id": "DOGE-USDT-SWAP", "leverage": 15, "notional": 500, "pos_side": "long"},
-    # {"inst_id": "SHIB-USDT-SWAP", "leverage": 15, "notional": 500, "pos_side": "long"},
-    # {"inst_id": "FLOKI-USDT-SWAP", "leverage": 15, "notional": 500, "pos_side": "long"},
-    # {"inst_id": "BONK-USDT-SWAP", "leverage": 15, "notional": 500, "pos_side": "long"},
-    # {"inst_id": "TURBO-USDT-SWAP", "leverage": 15, "notional": 500, "pos_side": "long"},
+    # {"inst_id": "ADA-USDT-SWAP", "leverage": 10, "notional": 500, "pos_side": "long"},
+    # {"inst_id": "SUI-USDT-SWAP", "leverage": 10, "notional": 500, "pos_side": "long"},
+    # {"inst_id": "AAVE-USDT-SWAP", "leverage": 10, "notional": 500, "pos_side": "long"},
+    # {"inst_id": "DOGE-USDT-SWAP", "leverage": 10, "notional": 500, "pos_side": "long"},
+    # {"inst_id": "SHIB-USDT-SWAP", "leverage": 10, "notional": 500, "pos_side": "long"},
+    # {"inst_id": "FLOKI-USDT-SWAP", "leverage": 10, "notional": 500, "pos_side": "long"},
+    # {"inst_id": "BONK-USDT-SWAP", "leverage": 10, "notional": 500, "pos_side": "long"},
+    # {"inst_id": "TURBO-USDT-SWAP", "leverage": 10, "notional": 500, "pos_side": "long"},
 ]
 
 # 批量处理多个币种
